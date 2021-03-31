51d6fd3b2d43d70acfeda7da13df43c0f97901e0
akashuplight <akash.raj@uplight.com>
2021-03-31 19:28:31 +0530

    lion-1057 created the REST API for enrollment status

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
-import os
+import os, requests, datetime, json
 
 from logging import getLogger
-from chalice import Chalice
+from chalice import Chalice, BadRequestError, NotFoundError, UnauthorizedError, ForbiddenError, ChaliceViewError
 
 from chalicelib.annotations.timer import timer
 from chalicelib.utils.datadog_utils import count
@@ -13,7 +13,9 @@ LOG_LEVEL = os.getenv('LOG_LEVEL', 'INFO')
 init_logging(SERVICE_NAME, LOG_LEVEL)
 LOGGER = getLogger(__name__)
 
-app = Chalice(app_name=SERVICE_NAME)
+app = Chalice(app_name=SERVICE_NAME, debug=True)
+
+BASE_URL = "https://dr-enrollment.mp.uat.simpleenergy.io/"
 
 
 # Be sure @timer is after all @app.* annotations, otherwise it won't work
@@ -23,6 +25,54 @@ def get_health():
     LOGGER.info('Health invoked.')
     count(metric='health.count', custom_a='tag', custom_b=1)
     return {'status': 'OK'}
+
+
+def updated_enrollment_status(data):
+    if "enrollment_state" in data and data["enrollment_state"] in ["enrolled" , "unenrolled"]:
+        return "unenrolled" if data["enrollment_state"] == "enrolled" else "enrolled"
+
+
+def handling_server_errors(status_code, type_req='GET'):
+    if status_code >=200 and status_code <300:
+        return
+    elif status_code is 404:
+        raise NotFoundError("Record not found!!")
+    elif status_code is 403:
+        raise ForbiddenError("Forbidden: You don't have permission to access!!")
+    elif status_code is 401:
+        raise UnauthorizedError("Unauthorized: You are not authorize!!!")
+    elif status_code is 400:
+        raise BadRequestError("BadRequest: 400")
+    else:
+        raise ChaliceViewError("Internal server error" if type_req=="GET" else "Internal error on POST request")
+
+
+@app.route("/enrollment", methods=['POST'])
+@timer('enrollment.response_time_ms', custom='tag')
+def enrollment_status():
+    body  = app.current_request.json_body
+    if body.get('token', None) and body.get('enrollment_uuid', None):
+        resp = requests.get(BASE_URL + "enrollment/" + body.get('enrollment_uuid'), headers={"x-api-key": body.get('token')})
+
+        handling_server_errors(resp.status_code)
+        data = resp.json()
+
+        # using  UTC time zone
+        payload = {
+            "enrollment_state": updated_enrollment_status(data),
+            "enrollment_date": datetime.datetime.utcnow().strftime("%Y-%m-%dT%H:%M:%S.%fZ")
+        }
+        utc_time = datetime.datetime.utcnow()
+        headers = {
+            'x-api-key': body.get('token'),
+            'content-type': "application/json"
+        }
+        resp = requests.post(BASE_URL + "enrollment/" + body.get('enrollment_uuid'), data=json.dumps(payload), headers=headers)
+        handling_server_errors(resp.status_code, 'POST')
+        return resp.json()
+
+    else:
+        raise BadRequestError(f"`token` and `enrollment_uuid` can't be empty!!")
 
 # Here are a few more examples:
 #
